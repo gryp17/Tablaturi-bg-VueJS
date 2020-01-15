076a6312e5592065e3f46f3bfa2a48455ea9c06c
plamen <plamen.ivanov17@gmail.com>
2020-01-15 12:04:24 +0200

    Added a few frontend libraries

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,14 @@
 
 		{{ userSession }}
 
-		<button v-if="!userSession" @click="login">
+		<button v-if="!userSession" @click="login" class="btn btn-success">
 			Login
 		</button>
-		<button v-else @click="logout">
+		<button v-else @click="logout" class="btn btn-danger">
 			Logout
 		</button>
 
-		<button @click="getUserSession">
+		<button @click="getUserSession" class="btn btn-info">
 			Get session
 		</button>
 
@@ -74,24 +74,24 @@
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+	}
 
-#nav {
-  padding: 30px;
+	#nav {
+		padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+		a {
+			font-weight: bold;
+			color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+			&.router-link-exact-active {
+				color: #42b983;
+			}
+		}
+	}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,28 @@
 import Vue from 'vue';
+import Toasted from 'vue-toasted';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+Vue.use(Toasted, {
+	position: 'bottom-right',
+	duration: 8000,
+	keepOnHover: true,
+	iconPack: 'fontawesome'
+});
+
+//register custom toast type
+/*
+Vue.toasted.register('apiError', (payload) => {
+	return payload.message || 'Something went wrong...';
+}, {
+	type: 'error',
+	icon: {
+		name: 'fa-exclamation-triangle'
+	}
+});
+*/
 
 Vue.config.productionTip = false;
 
