1fad34319f0677e9af77fc691fb7955f444cfe2a
Plamen Ivanov <plamen.ivanov@acronis.com>
2020-02-19 16:44:05 +0200

    Added the base for the profile page tabs

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,5 +1,101 @@
 <template>
-	<div>
-		Profile
+	<div class="profile-page">
+
+		<TabsNav>
+			<template v-slot:items>
+				<li class="nav-item">
+					<a
+						class="nav-link active"
+						id="profile-tab"
+						data-toggle="tab"
+						href="#profile"
+						role="tab"
+						aria-controls="profile"
+						aria-selected="true"
+					>
+						Профил
+					</a>
+				</li>
+				<li class="nav-item">
+					<a
+						class="nav-link"
+						id="user-tabs-tab"
+						data-toggle="tab"
+						href="#user-tabs"
+						role="tab"
+						aria-controls="user-tabs"
+						aria-selected="true"
+					>
+						Качени таблатури
+					</a>
+				</li>
+				<li class="nav-item">
+					<a
+						class="nav-link"
+						id="favourite-tabs-tab"
+						data-toggle="tab"
+						href="#favourite-tabs"
+						role="tab"
+						aria-controls="favourite-tabs"
+						aria-selected="true"
+					>
+						Любими таблатури
+					</a>
+				</li>
+				<li class="nav-item">
+					<a
+						class="nav-link"
+						id="user-search-tab"
+						data-toggle="tab"
+						href="#user-search"
+						role="tab"
+						aria-controls="user-search"
+						aria-selected="true"
+					>
+						Търси потребители
+					</a>
+				</li>
+				<li class="nav-item">
+					<a
+						class="nav-link"
+						id="add-article-tab"
+						data-toggle="tab"
+						href="#add-article"
+						role="tab"
+						aria-controls="add-article"
+						aria-selected="true"
+					>
+						Качи новина
+					</a>
+				</li>
+			</template>
+			<template v-slot:content>
+				<div class="tab-pane fade show active" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+					profile
+				</div>
+				<div class="tab-pane fade" id="user-tabs" role="tabpanel" aria-labelledby="user-tabs-tab">
+					user tabs
+				</div>
+				<div class="tab-pane fade" id="favourite-tabs" role="tabpanel" aria-labelledby="favourite-tabs-tab">
+					favourite tabs
+				</div>
+				<div class="tab-pane fade" id="user-search" role="tabpanel" aria-labelledby="user-search-tab">
+					user search
+				</div>
+				<div class="tab-pane fade" id="add-article" role="tabpanel" aria-labelledby="add-article-tab">
+					add article
+				</div>
+			</template>
+		</TabsNav>
 	</div>
 </template>
+
+<script>
+	import TabsNav from '@/components/TabsNav';
+
+	export default {
+		components: {
+			TabsNav
+		}
+	};
+</script>
--- src/views/Usefull.vue
+++ src/views/Usefull.vue
@@ -4,142 +4,147 @@
 			Полезно
 		</PageTitle>
 
-		<ul class="nav nav-tabs nav-fill" role="tablist">
-			<li class="nav-item">
-				<a
-					class="nav-link active"
-					id="software-tab"
-					data-toggle="tab"
-					href="#software"
-					role="tab"
-					aria-controls="software"
-					aria-selected="true"
-				>
-					Полезни програми
-				</a>
-			</li>
-			<li class="nav-item">
-				<a
-					class="nav-link"
-					id="lessons-tab"
-					data-toggle="tab"
-					href="#lessons"
-					role="tab"
-					aria-controls="lessons"
-					aria-selected="false"
-				>
-					Уроци за начинаещи
-				</a>
-			</li>
-			<li class="nav-item">
-				<a
-					class="nav-link"
-					id="video-lessons-tab"
-					data-toggle="tab"
-					href="#video-lessons"
-					role="tab"
-					aria-controls="activity"
-					aria-selected="false"
-				>
-					Видео уроци
-				</a>
-			</li>
-		</ul>
+		<TabsNav :flex="true">
+			<template v-slot:items>
+				<li class="nav-item">
+					<a
+						class="nav-link active"
+						id="software-tab"
+						data-toggle="tab"
+						href="#software"
+						role="tab"
+						aria-controls="software"
+						aria-selected="true"
+					>
+						Полезни програми
+					</a>
+				</li>
+				<li class="nav-item">
+					<a
+						class="nav-link"
+						id="lessons-tab"
+						data-toggle="tab"
+						href="#lessons"
+						role="tab"
+						aria-controls="lessons"
+						aria-selected="false"
+					>
+						Уроци за начинаещи
+					</a>
+				</li>
+				<li class="nav-item">
+					<a
+						class="nav-link"
+						id="video-lessons-tab"
+						data-toggle="tab"
+						href="#video-lessons"
+						role="tab"
+						aria-controls="activity"
+						aria-selected="false"
+					>
+						Видео уроци
+					</a>
+				</li>
+			</template>
+			<template v-slot:content>
+				<!-- software tab -->
+				<div class="tab-pane fade show active" id="software" role="tabpanel" aria-labelledby="software-tab">
+					<div class="usefull-item">
+						<img src="/img/usefull/tuxguitar.png" />
+						<div class="description">
+							<h5>
+								<a class="red" href="http://www.tuxguitar.com.ar/download.html" target="_blank">TuxGuitar</a>
+							</h5>
+							TuxGuitar e безплатна алтернатива на известната програма за таблатури и партитури - Guitar Pro.
+							Софтуерът служи за създаване, редактиране и преслушване на таблатури.
+							С него можете да отваряте и обработвате всички файлове генерирани от Guitar Pro - .tg, .gp3, gp.4, .gp5, .ptb.
+						</div>
+					</div>
 
-		<div class="tab-content">
-			<!-- software tab -->
-			<div class="tab-pane fade show active" id="software" role="tabpanel" aria-labelledby="software-tab">
-				<div class="usefull-item">
-					<img src="/img/usefull/tuxguitar.png" />
-					<div class="description">
-						<h5>
-							<a class="red" href="http://www.tuxguitar.com.ar/download.html" target="_blank">TuxGuitar</a>
-						</h5>
-						TuxGuitar e безплатна алтернатива на известната програма за таблатури и партитури - Guitar Pro.
-						Софтуерът служи за създаване, редактиране и преслушване на таблатури.
-						С него можете да отваряте и обработвате всички файлове генерирани от Guitar Pro - .tg, .gp3, gp.4, .gp5, .ptb.
+					<div class="usefull-item">
+						<img src="/img/usefull/aptuner.jpg" />
+						<div class="description">
+							<h5>
+								<a class="red" href="https://ap-tuner.en.softonic.com/" target="_blank">APTuner</a>
+							</h5>
+							APTuner е безплатна програма за бързо и лесно настройване на китара като може да се използва както на компютър, така и на таблет или смартфон.
+						</div>
+					</div>
+
+					<div class="usefull-item">
+						<img src="/img/usefull/audacity.jpg" />
+						<div class="description">
+							<h5>
+								<a class="red" href="http://audacity.sourceforge.net/download/" target="_blank">Audacity</a>
+							</h5>
+							Audacity e безплатна програма за обработка и запис на аудио. Може да се използва и като виртуален усилвател с подходящите за целта plugin-и.
+						</div>
 					</div>
 				</div>
-
-				<div class="usefull-item">
-					<img src="/img/usefull/aptuner.jpg" />
-					<div class="description">
-						<h5>
-							<a class="red" href="https://ap-tuner.en.softonic.com/" target="_blank">APTuner</a>
-						</h5>
-						APTuner е безплатна програма за бързо и лесно настройване на китара като може да се използва както на компютър, така и на таблет или смартфон.
+				<!-- lessons tab -->
+				<div class="tab-pane fade" id="lessons" role="tabpanel" aria-labelledby="lessons-tab">
+					<div class="usefull-item">
+						<img src="/img/usefull/lessons.jpg" />
+						<div class="description">
+							<h5>
+								<a class="red" :href="`${CDN_URL}/downloads/lessons.pdf`">Уроци по китара (Скот Фритцингер)</a>
+							</h5>
+							Уроците включват основна теория по китара, техника за свирене, както и информация за акордите, техните прогресии и вариации.
+						</div>
+					</div>
+					<div class="usefull-item">
+						<img src="/img/usefull/tab.jpg" />
+						<div class="description">
+							<h5>
+								<a class="red" :href="`${CDN_URL}/downloads/tabs.pdf`">Писане и четене на таблатури (Хауърд Райт)</a>
+							</h5>
+							Урокът обяснява какво представляват таблатурите, как се четат и пишат и най-често използваните символи.
+						</div>
 					</div>
 				</div>
+				<!-- video lessons tab -->
+				<div class="tab-pane fade" id="video-lessons" role="tabpanel" aria-labelledby="video-lessons-tab">
 
-				<div class="usefull-item">
-					<img src="/img/usefull/audacity.jpg" />
-					<div class="description">
-						<h5>
-							<a class="red" href="http://audacity.sourceforge.net/download/" target="_blank">Audacity</a>
-						</h5>
-						Audacity e безплатна програма за обработка и запис на аудио. Може да се използва и като виртуален усилвател с подходящите за целта plugin-и.
+					<h5>
+						<a class="red" href="http://www.guitarjamz.com" target="_blank">Видео уроци от Марти Шварц</a>
+					</h5>
+
+					<div class="videos-wrapper">
+						<iframe src="http://www.youtube.com/embed/?listType=user_uploads&list=guitarjamzdotcom" width="340" height="390"></iframe>
+						<iframe src="http://www.youtube.com/embed/?listType=user_uploads&list=martyzsongs" width="340" height="390"></iframe>
 					</div>
-				</div>
-			</div>
-			<!-- lessons tab -->
-			<div class="tab-pane fade" id="lessons" role="tabpanel" aria-labelledby="lessons-tab">
-				<div class="usefull-item">
-					<img src="/img/usefull/lessons.jpg" />
-					<div class="description">
-						<h5>
-							<a class="red" :href="`${CDN_URL}/downloads/lessons.pdf`">Уроци по китара (Скот Фритцингер)</a>
-						</h5>
-						Уроците включват основна теория по китара, техника за свирене, както и информация за акордите, техните прогресии и вариации.
+
+					<hr/>
+
+					<h5>
+						Още видео уроци
+					</h5>
+
+					<div class="videos-wrapper">
+						<iframe src="http://www.youtube.com/embed/?listType=user_uploads&list=JustinSandercoeSongs" width="340" height="390"></iframe>
+						<iframe src="http://www.youtube.com/embed/?listType=user_uploads&list=guitarlessonscom" width="340" height="390"></iframe>
+
+						<iframe src="http://www.youtube.com/embed/?listType=user_uploads&list=tripfuse04" width="340" height="390"></iframe>
+						<iframe src="http://www.youtube.com/embed/?listType=user_uploads&list=educationalmaterials" width="340" height="390"></iframe>
+
+						<iframe src="http://www.youtube.com/embed/?listType=user_uploads&list=KristoferDahl" width="340" height="390"></iframe>
+						<iframe src="http://www.youtube.com/embed/?listType=user_uploads&list=SteveTerreberry" width="340" height="390"></iframe>
 					</div>
+
 				</div>
-				<div class="usefull-item">
-					<img src="/img/usefull/tab.jpg" />
-					<div class="description">
-						<h5>
-							<a class="red" :href="`${CDN_URL}/downloads/tabs.pdf`">Писане и четене на таблатури (Хауърд Райт)</a>
-						</h5>
-						Урокът обяснява какво представляват таблатурите, как се четат и пишат и най-често използваните символи.
-					</div>
-				</div>
-			</div>
-			<!-- video lessons tab -->
-			<div class="tab-pane fade" id="video-lessons" role="tabpanel" aria-labelledby="video-lessons-tab">
-
-				<h5>
-					<a class="red" href="http://www.guitarjamz.com" target="_blank">Видео уроци от Марти Шварц</a>
-				</h5>
-
-				<div class="videos-wrapper">
-					<iframe src="http://www.youtube.com/embed/?listType=user_uploads&list=guitarjamzdotcom" width="340" height="390"></iframe>
-					<iframe src="http://www.youtube.com/embed/?listType=user_uploads&list=martyzsongs" width="340" height="390"></iframe>
-				</div>
-
-				<hr/>
-
-				<h5>
-					Още видео уроци
-				</h5>
-
-				<div class="videos-wrapper">
-					<iframe src="http://www.youtube.com/embed/?listType=user_uploads&list=JustinSandercoeSongs" width="340" height="390"></iframe>
-					<iframe src="http://www.youtube.com/embed/?listType=user_uploads&list=guitarlessonscom" width="340" height="390"></iframe>
-
-					<iframe src="http://www.youtube.com/embed/?listType=user_uploads&list=tripfuse04" width="340" height="390"></iframe>
-					<iframe src="http://www.youtube.com/embed/?listType=user_uploads&list=educationalmaterials" width="340" height="390"></iframe>
-
-					<iframe src="http://www.youtube.com/embed/?listType=user_uploads&list=KristoferDahl" width="340" height="390"></iframe>
-					<iframe src="http://www.youtube.com/embed/?listType=user_uploads&list=SteveTerreberry" width="340" height="390"></iframe>
-				</div>
-
-			</div>
-		</div>
+			</template>
+		</TabsNav>
 	</div>
 </template>
 
 <script>
 	import { mapState } from 'vuex';
+	import TabsNav from '@/components/TabsNav';
 
 	export default {
+		components: {
+			TabsNav
+		},
 		computed: {
 			...mapState([
 				'CDN_URL'
@@ -150,19 +155,6 @@
 
 <style scoped lang="scss">
 	.usefull-page {
-
-		.nav-item {
-			flex: 1;
-
-			.nav-link {
-				color: $text-color;
-
-				&.active {
-					font-weight: bold;
-					color: $red;
-				}
-			}
-		}
 
 		.usefull-item {
 			display: flex;
