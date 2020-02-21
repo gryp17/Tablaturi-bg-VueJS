54515a3373e43ab41f1216af11a0fa941cbd11c9
Plamen Ivanov <plamen.ivanov@acronis.com>
2020-02-21 15:45:59 +0200

    Added the basic user info section

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -20,11 +20,62 @@
 			</FormButton>
 		</div>
 
+		<div class="table-wrapper">
+			<img :src="`${CDN_URL}/avatars/${user.photo}`" class="user-avatar" />
+
+			<div class="table-wrapper-inner">
+				<table class="table table-striped">
+					<tbody>
+						<tr>
+							<td>
+								<span class="info-label">{{ registeredText }}</span>
+								{{ registeredDate }}
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<span class="info-label">{{ lastActiveText }}</span>
+								{{ lastActiveDate }}
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<span class="info-label">Възраст:</span>
+								{{ age }} години
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<span class="info-label">Местоживеене:</span>
+								{{ user.location || 'Няма информация' }}
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<span class="info-label">Професия:</span>
+								{{ user.occupation || 'Няма информация' }}
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<span class="info-label">Web:</span>
+								<a v-if="user.web" :href="user.web" class="red" target="_blank">{{ user.web }}</a>
+								<template v-else>
+									Няма информация
+								</template>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+
 		{{ user }}
 	</div>
 </template>
 
 <script>
+	import moment from 'moment';
 	import { mapState } from 'vuex';
 	import GenderIcon from '@/components/GenderIcon';
 
@@ -39,6 +90,24 @@
 			...mapState('auth', [
 				'userSession'
 			]),
+			...mapState([
+				'CDN_URL'
+			]),
+			registeredText() {
+				return this.user.gender === 'M' ? 'Регистриран на:' : 'Регистриранa на:';
+			},
+			registeredDate() {
+				return moment(this.user.register_date).format('YYYY-MM-DD в HH:mm');
+			},
+			lastActiveText() {
+				return this.user.gender === 'M' ? 'Последно активен на:' : 'Последно активна на:';
+			},
+			lastActiveDate() {
+				return moment(this.user.last_active_date).format('YYYY-MM-DD в HH:mm');
+			},
+			age() {
+				return moment().diff(moment(this.user.birthday), 'years');
+			},
 			isAdmin() {
 				return this.user.type === 'admin';
 			},
@@ -58,6 +127,38 @@
 
 			.edit-btn, .report-btn {
 				float: right;
+			}
+		}
+
+		.table-wrapper {
+			display: flex;
+			margin-top: 20px;
+
+			.user-avatar {
+				align-self: center;
+				margin-right: 20px;
+				width: 200px;
+				height: 200px;
+				border-radius: 100%;
+			}
+
+			.table-wrapper-inner {
+				flex: 1;
+				padding-left: 20px;
+				border-left: solid 3px $red;
+
+				.table {
+					margin-bottom: 0;
+
+					td {
+						padding: 10px;
+
+						.info-label {
+							margin-right: 5px;
+							color: $red;
+						}
+					}
+				}
 			}
 		}
 	}
